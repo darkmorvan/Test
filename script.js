f1aade2d73675309041cb7db88832ebee25c1e02
Alexey Ezlov <alexgezlov@gmail.com>
2017-04-28 16:22:39 +0300

    скрипт для показа изображения

---
+++ script.js
@@ -0,0 +1,84 @@
+$(function(){
+	
+	var dropbox = $('#dropbox'),
+		message = $('.message', dropbox);
+	
+	dropbox.filedrop({
+		paramname:'pic',
+		maxfiles: 5,
+    	maxfilesize: 2,
+		url: 'post_file.php',
+		
+		uploadFinished:function(i,file,response){
+			$.data(file).addClass('done');
+		},
+		
+    	error: function(err, file) {
+			switch(err) {
+				case 'BrowserNotSupported':
+					showMessage('Your browser does not support HTML5 file uploads!');
+					break;
+				case 'TooManyFiles':
+					alert('Too many files! Please select 5 at most!');
+					break;
+				case 'FileTooLarge':
+					alert(file.name+' is too large! Please upload files up to 2mb (configurable).');
+					break;
+				default:
+					break;
+			}
+		},
+		
+		beforeEach: function(file){
+			if(!file.type.match(/^image\//)){
+				alert('Only images are allowed!');
+				return false;
+			}
+		},
+		
+		uploadStarted:function(i, file, len){
+			createImage(file);
+		},
+		
+		progressUpdated: function(i, file, progress) {
+			$.data(file).find('.progress').width(progress);
+		}
+    	 
+	});
+	
+	var template = '<div class="preview">'+
+						'<span class="imageHolder">'+
+							'<img />'+
+							'<span class="uploaded"></span>'+
+						'</span>'+
+						'<div class="progressHolder">'+
+							'<div class="progress"></div>'+
+						'</div>'+
+					'</div>'; 
+	
+	
+	function createImage(file){
+
+		var preview = $(template), 
+			image = $('img', preview);
+			
+		var reader = new FileReader();
+		
+		image.width = 100;
+		image.height = 100;
+		
+		reader.onload = function(e)
+		{
+		image.attr('src',e.target.result);
+		};
+		reader.readAsDataURL(file);
+		message.hide();
+		preview.appendTo(dropbox);
+		$.data(file,preview);
+	}
+
+	function showMessage(msg){
+		message.html(msg);
+	}
+
+});
